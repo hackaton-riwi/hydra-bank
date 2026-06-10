be5a3ffa6f4eb7f670df39943f6354e0e090e8a8
Duvan Estiven Mona <duvanestivenpiedrahita>
2026-06-09 21:03:38 -0500

    ok, login and register correct

--- Hydra.Application/DTOs/CreateRoleDto.cs
+++ Hydra.Application/DTOs/CreateRoleDto.cs
@@ -1,6 +1,9 @@
 namespace Hydra.Application.DTOs;
 
+using System.ComponentModel.DataAnnotations;
+
 public class CreateRoleDto
 {
+    [Required]
     public string Name { get; set; } = string.Empty;
 }
--- Hydra.Application/DTOs/RegisterDto.cs
+++ Hydra.Application/DTOs/RegisterDto.cs
@@ -1,7 +1,14 @@
 namespace Hydra.Application.DTOs;
 
+using System.ComponentModel.DataAnnotations;
+
 public class RegisterDto
 {
+    [Required]
+    [EmailAddress]
     public string Email { get; set; } = string.Empty;
+
+    [Required]
+    [MinLength(6)]
     public string Password { get; set; } = string.Empty;
 }
